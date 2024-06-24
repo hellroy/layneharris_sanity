743734383eaf9f4609189a53b9d719abf58f21ca
Layne Harris <helsmack@gmail.com>
2024-06-24 14:03:52 -0400

    testing private variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,17 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
+    sanity: {
+      projectId: process.env.NUXT_SANITY_PROJECT_ID,
+      dataset: process.env.NUXT_SANITY_DATASET,
+      useCdn: true,
+      apiVersion: process.env.NUXT_SANITY_API_VERSION || '2024-03-15',
+      visualEditing: {
+        studioUrl: process.env.NUXT_SANITY_STUDIO_URL || 'http://localhost:3333',
+        token: process.env.NUXT_SANITY_API_READ_TOKEN,
+        stega: true,
+      },
+    },
     public: {
       sanity: {
         projectId: process.env.NUXT_SANITY_PROJECT_ID,
