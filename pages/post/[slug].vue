481819bfad38404d42553a1eb4aa15d3df0a3037
Layne Harris <helsmack@gmail.com>
2024-07-02 12:13:33 -0400

    changes to blog section
    style changes

--- pages/post/[slug].vue
+++ pages/post/[slug].vue
@@ -12,15 +12,16 @@ const { data: post } = await useSanityQuery(query, {
 
 <template>
   <section v-if="post" class="post w-full my-4 md:my-16 px-4 md:py-4">
-    <div class="m-auto text-center">
+    <div class="m-auto text-center px-4">
     <img
       v-if="post.coverImage"
-      class="post__cover w-full h-52 object-cover md:w-[750px] md:h-[380px] rounded-lg m-auto drop-shadow-[0_5px_5px_rgba(230,245,39,.8)]"
+      class="post__cover w-full object-cover md:w-[750px] md:h-[500px] rounded-lg m-auto drop-shadow-[0_5px_5px_rgba(230,245,39,.8)]"
       :src="urlFor(post.coverImage).width(1920).url()"
       alt="Cover image"
     />
     <div v-else class="post__cover--none w-full h-52 md:w-[750px] md:h-[380px]" />
-    <div class="post__container px-6 md:px-12 w-[75%] m-auto">
+    <div class="m-auto md:w-[75%] py-8">
+    <div class="post__container px-6 md:px-12 text-left">
       <h1 class="post__title text-3xl md:text-5xl  font-headings text-layneYellow leading-tight mt-4 md:mt-8 mb-4 md:mb-6">{{ post.title }}</h1>
       <p class="post__excerpt font-sans text-white text-xl md:text-2xl leading-snug mt-0">{{ post.excerpt }}</p>
       <p class="post__date font-sans text-layneBlue font-semibold text-xs md:text-lg mt-4">{{ formatDate(post._createdAt) }}</p>
@@ -28,6 +29,7 @@ const { data: post } = await useSanityQuery(query, {
         <PortableText :value="post.body" />
       </div>
     </div>
+  </div>
   </div>
   </section>
 </template>
