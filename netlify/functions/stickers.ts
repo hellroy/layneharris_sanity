674052b1740ce9ce3ab1c550a192d283546cb0b2
Layne Harris <helsmack@gmail.com>
2024-07-23 10:56:58 -0400

    Update stickers.ts

--- netlify/functions/stickers.ts
+++ netlify/functions/stickers.ts
@@ -7,11 +7,21 @@ exports.handler = async function(event, context) {
     const stickers = await client.fetch(query);
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow all origins
+        'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+        'Access-Control-Allow-Methods': 'GET', // Allow specific methods
+      },
       body: JSON.stringify(stickers)
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow all origins
+        'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+        'Access-Control-Allow-Methods': 'GET', // Allow specific methods
+      },
       body: JSON.stringify({ error: 'Failed to fetch stickers' })
     };
   }
